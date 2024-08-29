828005cfc2bf4a1ad128bc95b7b3533be6b5a6a5
Rabeya <167190125+rabeyakhatun007@users.noreply.github.com>
2024-08-29 18:48:10 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,75 @@
+
+//====================JS========================
+var countdown= $("#countdown").countdown360({
+    radius: 50.5,
+    strokeStyle:"#477050",
+    strokeWidth: undefined,
+    fillStyle:"#8ac575",
+    fontColor:"#477050",
+    fontFamily:"sans-serif",
+    fontSize: undefined,
+    fontWeight: 700,
+    autostart:true,
+    seconds: 100,
+    label: ["second","seconds"],
+    startOverAfterAdding:true,
+    smooth:false,
+    onComplete:function () {}
+});
+
+var countdown= $("#countdown1").countdown360({
+    radius: 50.5,
+    strokeStyle:"#477050",
+    strokeWidth: undefined,
+    fillStyle:"#8ac575",
+    fontColor:"#477050",
+    fontFamily:"sans-serif",
+    fontSize: undefined,
+    fontWeight: 700,
+    autostart:true,
+    seconds: 100,
+    label: ["second","seconds"],
+    startOverAfterAdding:true,
+    smooth:false,
+    onComplete:function () {}
+});
+
+var countdown= $("#countdown2").countdown360({
+    radius: 50.5,
+    strokeStyle:"#477050",
+    strokeWidth: undefined,
+    fillStyle:"#8ac575",
+    fontColor:"#477050",
+    fontFamily:"sans-serif",
+    fontSize: undefined,
+    fontWeight: 700,
+    autostart:true,
+    seconds: 100,
+    label: ["second","seconds"],
+    startOverAfterAdding:true,
+    smooth:false,
+    onComplete:function () {}
+});
+
+
+
+
+
+
+
+
+
+
+//====================JQ========================
+
+$(document).ready(function(){
+
+//counterup
+$('.counter').counterUp({
+    'time': 2000,
+    'delay': 10
+});
+
+
+
+});
---
+++ vendor/counting/README.md
@@ -0,0 +1,15 @@
+# Counter
+Add counter to your site using this jQuery plugin.
+
+## Usage  
+```JS
+jQuery(function($) {
+    $('.count').countTo({
+        from: 2.50,
+        to: 0.91,
+        speed: 700,
+	decimals: 2,
+	separator: ','
+    });
+});
+```
